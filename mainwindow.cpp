de153f9e59abe507ff24c364dbed59ae4bee46f1
dive155 <divedimon@mail.ru>
2016-03-26 18:15:37 +0300

    It's done. SUAI, what the heck were you thinking?

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,7 +10,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
     fireEx.setRoom(&room);
     tresPas.setRoom(&room);
-
+    ui->label->setPixmap(QPixmap(":images/house.png"));
     connect(ui->fireButton, SIGNAL(clicked()),
             this, SLOT(startFire()));
     connect(ui->onFButton, SIGNAL(clicked()),
@@ -39,24 +39,24 @@ void MainWindow::startFire()
     room.setOnFire(1);
     drawAll();
     qApp->processEvents();
-    QThread::sleep(1);
+    QThread::sleep(3);
     checkFire();
 }
 
 void MainWindow::addIntruder()
 {
     room.intrude(1);
-    ui->intruder->setText("D");
+    //ui->intruder->setText("D");
     drawAll();
     qApp->processEvents();
-    QThread::sleep(1);
+    QThread::sleep(3);
     checkIntruder();
 }
 
 void MainWindow::removeIntruder()
 {
     room.intrude(0);
-    ui->intruder->setText("_");
+    //ui->intruder->setText("_");
     drawAll();
 }
 
@@ -66,13 +66,13 @@ void MainWindow::checkFire()
     {
         fireEx.activateSprinklers(1);
         drawAll();
-        ui->textEdit->append("Warning! Your house is on fire!");
+        ui->textEdit->append("Внимание! Ваш дом горит!");
         qApp->processEvents();
-        QThread::sleep(1);
+        QThread::sleep(3);
         room.setOnFire(0);
         drawAll();
         qApp->processEvents();
-        QThread::sleep(1);
+        QThread::sleep(3);
         fireEx.activateSprinklers(0);
         drawAll();
     }
@@ -84,18 +84,18 @@ void MainWindow::checkFire()
 
 void MainWindow::drawAll()
 {
-    if (room.isBurning()) ui->fireLabel->setText("_11_");
-    if (!room.isBurning()) ui->fireLabel->setText("____");
+    if (room.isBurning()) ui->fireLabel->setPixmap(QPixmap(":images/fire2.png"));//setText("_11_");
+    if (!room.isBurning()) ui->fireLabel->setPixmap(QPixmap(":images/fire1.png"));//setText("____");
 
-    if (fireEx.isSprinkling()) ui->sprinkLabel->setText("1111");
-    if (!fireEx.isSprinkling()) ui->sprinkLabel->setText("----");
-    if (fireEx.isOn()) ui->fireExLabel->setText("1");
-    else ui->fireExLabel->setText("0");
+    if (fireEx.isSprinkling()) ui->sprinkLabel->setPixmap(QPixmap(":images/sprink2.png"));//setText("1111");
+    if (!fireEx.isSprinkling()) ui->sprinkLabel->setPixmap(QPixmap(":images/sprink1.png"));//setText("----");
+    if (fireEx.isOn()) ui->fireExLabel->setPixmap(QPixmap(":images/butOn.png"));//setText("1");
+    else ui->fireExLabel->setPixmap(QPixmap(":images/butOff.png"));//setText("0");
 
-    if (room.isIntruded()) ui->intruder->setText("D");
-    if (!room.isIntruded()) ui->intruder->setText("_");
-    if (tresPas.isOn()) ui->tresPasLabel->setText("1");
-    else ui->tresPasLabel->setText("0");
+    if (room.isIntruded()) ui->intruder->setPixmap(QPixmap(":images/intruder2.png"));//setText("D");
+    if (!room.isIntruded()) ui->intruder->setPixmap(QPixmap(":images/intruder1.png"));;
+    if (tresPas.isOn()) ui->tresPasLabel->setPixmap(QPixmap(":images/butOn.png"));//setText("1");
+    else ui->tresPasLabel->setPixmap(QPixmap(":images/butOff.png"));//setText("0");
 }
 
 void MainWindow::turnOnFire()
@@ -129,6 +129,6 @@ void MainWindow::checkIntruder()
 {
     if (tresPas.intruderDetected() && tresPas.isOn())
     {
-        ui->textEdit->append("Warning! An intruder detected!");
+        ui->textEdit->append("Внимание! В ваш дом проникли!");
     }
 }
